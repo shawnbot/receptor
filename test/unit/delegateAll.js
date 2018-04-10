3e8133d57d5ad3029b0af04f7d4c8c248c161566
Shawn Allen <shawn.allen@github.com>
2018-04-10 11:09:20 -0700

    refactor unit tests as es2015, prettier

--- test/unit/delegateAll.js
+++ test/unit/delegateAll.js
@@ -1,81 +1,64 @@
-
-const assert = require('assert');
-const jsdom = require('jsdom-global');
-
-var cleanup = jsdom();
-const delegateAll = require('../../delegateAll');
-cleanup();
-
-beforeEach(function() {
-  cleanup = jsdom();
-});
-
-afterEach(function() {
-  cleanup();
-});
+import assert from 'assert'
+import delegateAll from '../../delegateAll'
 
 describe('delegateAll({selector: function})', function() {
-
   it('delegates to the target', function(done) {
-    document.body.innerHTML = '<div><a>foo</a></div>';
-    var link = document.querySelector('a');
+    document.body.innerHTML = '<div><a>foo</a></div>'
+    var link = document.querySelector('a')
     var listener = delegateAll({
-      'a': function(e) {
-        assert.strictEqual(this, link);
+      a: function(e) {
+        assert.strictEqual(this, link)
       },
-      'b': function(e) {
-        assert.ok(false, 'this listener should not fire');
-      }
-    });
-    document.body.addEventListener('click', listener);
-    link.click();
-    document.body.removeEventListener('click', listener);
-    process.nextTick(done);
-  });
-
-});
+      b: function(e) {
+        assert.ok(false, 'this listener should not fire')
+      },
+    })
+    document.body.addEventListener('click', listener)
+    link.click()
+    document.body.removeEventListener('click', listener)
+    process.nextTick(done)
+  })
+})
 
 describe('delegateAll() with multiple selectors', function() {
-
   it('delegates to multiple callbacks', function(done) {
-    document.body.innerHTML = '<div><a>foo <b>bar</b></a></div>';
-    var values = [];
+    document.body.innerHTML = '<div><a>foo <b>bar</b></a></div>'
+    var values = []
     var listener = delegateAll({
-      'a': function() {
-        values.push(this.nodeName);
+      a: function() {
+        values.push(this.nodeName)
       },
-      'b': function(e) {
-        values.push(this.nodeName);
-      }
-    });
-    document.body.addEventListener('click', listener);
-    document.querySelector('b').click();
-    document.body.removeEventListener('click', listener);
+      b: function(e) {
+        values.push(this.nodeName)
+      },
+    })
+    document.body.addEventListener('click', listener)
+    document.querySelector('b').click()
+    document.body.removeEventListener('click', listener)
     process.nextTick(function() {
-      assert.deepEqual(values, ['A', 'B']);
-      done();
-    });
-  });
+      assert.deepEqual(values, ['A', 'B'])
+      done()
+    })
+  })
 
   it('short-circuits when one callback returns false', function(done) {
-    document.body.innerHTML = '<div><a>foo <b>bar</b></a></div>';
-    var values = [];
+    document.body.innerHTML = '<div><a>foo <b>bar</b></a></div>'
+    var values = []
     var listener = delegateAll({
-      'a': function() {
-        values.push(this.nodeName);
-        return false;
+      a: function() {
+        values.push(this.nodeName)
+        return false
       },
-      'b': function(e) {
-        values.push(this.nodeName);
-      }
-    });
-    document.body.addEventListener('click', listener);
-    document.querySelector('b').click();
-    document.body.removeEventListener('click', listener);
+      b: function(e) {
+        values.push(this.nodeName)
+      },
+    })
+    document.body.addEventListener('click', listener)
+    document.querySelector('b').click()
+    document.body.removeEventListener('click', listener)
     process.nextTick(function() {
-      assert.deepEqual(values, ['A']);
-      done();
-    });
-  });
-
-});
+      assert.deepEqual(values, ['A'])
+      done()
+    })
+  })
+})
--- test/unit/ignore.js
+++ test/unit/ignore.js
@@ -1,31 +1,21 @@
-const assert = require('assert');
-const jsdom = require('jsdom-global');
+import assert from 'assert'
+import ignore from '../../ignore'
 
-const ignore = require('../../ignore');
-
-beforeEach(function() {
-  cleanup = jsdom();
-});
-
-afterEach(function() {
-  cleanup();
-});
+const SELECTORS = ['body', 'button', 'a', 'div']
 
 describe('ignore(element, function)', function() {
-
   it('ignores the target', function() {
-    document.body.innerHTML = '<button>hi</button><div><a>foo</a></div>';
-    var target = document.querySelector('div');
-    var times = 0;
+    document.body.innerHTML = '<button>hi</button><div><a>foo</a></div>'
+    var target = document.querySelector('div')
+    var times = 0
     var listener = ignore(target, function(e) {
-      times++;
-    });
-    document.body.addEventListener('click', listener);
-    ['body', 'button', 'a', 'div'].forEach(function(selector) {
-      document.querySelector(selector).click();
-    });
-    assert.equal(times, 2);
-    document.body.removeEventListener('click', listener);
-  });
-
-});
+      times++
+    })
+    document.body.addEventListener('click', listener)
+    for (const selector of SELECTORS) {
+      document.querySelector(selector).click()
+    }
+    assert.equal(times, 2)
+    document.body.removeEventListener('click', listener)
+  })
+})
