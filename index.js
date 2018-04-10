0499c5845147a7a8324362a0103d0e8d607548bc
Shawn Allen <shawn.allen@github.com>
2018-04-10 12:33:02 -0700

    remove unused closest() export

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 import behavior from './src/behavior'
-import closest from './src/closest'
 import delegate from './src/delegate'
 import delegateAll from './src/delegateAll'
 import ignore from './src/ignore'
 import keymap from './src/keymap'
 import once from './src/once'
 
-export {behavior, closest, delegate, delegateAll, ignore, keymap, once}
+export {behavior, delegate, delegateAll, ignore, keymap, once}
--- src/closest/index.js
+++
@@ -1,9 +0,0 @@
-import matches from 'matches-selector'
-
-export default function closest(element, selector) {
-  do {
-    if (matches(element, selector)) {
-      return element
-    }
-  } while ((element = element.parentNode) && element.nodeType === 1)
-}
